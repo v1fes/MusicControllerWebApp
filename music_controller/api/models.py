41e420a492f06be123600cdb171b79d4bb1f1d6f
Fesenko Viktoria <vt221_fvv@student.ztu.edu.ua>
2025-01-29 00:51:37 +0200

    React Router and Building Components

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -17,5 +17,3 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip=models.IntegerField(null=False, default=1)
     created_at=models.DateTimeField(auto_now_add=True)
-
-
---
+++ music_controller/frontend/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("development"),
+      },
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
