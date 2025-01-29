8969130d7f916a442270a749d082a0adf255559c
Fesenko Viktoria <vt221_fvv@student.ztu.edu.ua>
2025-01-29 23:17:42 +0200

    Add React Router and API POST handling

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<p>This is the home page</p>}/>
+          <Route path="/join" element={<RoomJoinPage/>} />
+          <Route path="/create" element={<CreateRoomPage/>} />
+        </Routes>
+      </Router>
+    );
+  }
+}
